8a30e80c2e96c038872282940a7d8dcd9bd2347a
DaedalusDev <dev.daedalus@gmail.com>
2017-06-19 12:40:18 +0200

    First version

--- README.md
+++ README.md
@@ -2,3 +2,83 @@
 
 ##Installation
 
+### git
+`git clone https://github.com/DaedalusDev/materialize-clockpicker.git`
+# Api Doc
+## Options:
+### Full Default configuration
+Here are some options and their defaults:
+``` javascript
+datepicker: {
+    icon: "&#xE8DF;",       // Icon for distinct picker
+    label: "Date",          // Label for distinct picker
+    container: "body"       // Fix hidden/scroll... issue
+},
+timepicker: {
+    icon: "&#xE192;",       // Icon for distinct picker
+    label: "Time",          // Label for distinct picker
+    container: "body",      // Fix hidden/scroll... issue
+    twelvehour: false       // set pickatime doc
+},
+distinct: true,             // Double picker : true, single picker : false
+step: true,                 // Got to timepicker after pickadate
+
+min: false,                 // format : [{pickadate.min}, {pickatime.min}]
+max: false,                 // format : [{pickadate.max}, {pickatime.max}]
+disable: false              // format : [[{pickadate.format},{pickatime.format}], [{pickadate.format},[{pickatime.format}, {pickatime.format}, ...]], ... ]
+```
+### Setting defaults
+If you want to change the global default fonctionnality from this plugin, you can access default settings using $.fn.pickatime.defaults. 
+Example:
+``` javascript
+$.fn.pickadatetime.defaults.step = false;      // single change
+$.extend( true, $.fn.pickadatetime.defaults, {  // Multiple change
+    distinct: false,
+});
+```
+## Usage
+### At plugin init
+``` javascript
+$('#myInput').pickadatetime({
+    distinct: false
+});
+```
+### After plugin init
+You can get the clockpicker instance :
+``` javascript
+$('#myInput').pickadatetime();          // Method 1
+$('#myInput').data('datetimepicker');  // Method 2
+```
+#### Change some options
+``` javascript
+// Simply recall apply new options on instance
+$('#myInput')
+    .pickadatetime({
+        distinct: false
+    });
+    
+// With set method
+// Method 1 :
+$('#myInput')
+    .pickadatetime('set','distinct', false) // set individual change
+    .pickadatetime('set', {                 // multiple changes
+        distinct: false
+    });
+ 
+// Method 2 :
+$('#myInput')
+    .pickadatetime()
+        .set('distinct', false)  // set individual change
+        .set({                  // multiple changes
+            min: [[2017, 5, 19],[8, 30]]
+        });
+```
+
+### Time format handler
+The plugin can handle all date and time format supported from pickadate et pickatime
+
+## Developing:
+```
+npm install
+// grunt monitor
+```
--- src/js/pickadatetime.js
+++ src/js/pickadatetime.js
@@ -1,4 +1,4 @@
-(function($, Picker){
+(function($){
     var idPrefix = 'datepicker_';
     var id = 0;
     function getuId() {
@@ -44,61 +44,95 @@
         }
     }
 
+    var aRestrinction = ['min','max','disable'];
+
     function DateTimePicker( $picker, options ) {
-        var uId = this.uId = getuId();
+        this.uId = getuId();
         var self = this;
         this.element = $picker;
         this.options = options;
 
         this.aDate = [];
 
-        this.$datePicker = $('<input type="text" id="datepicker_'+ idPrefix + uId +'" >');
+        this.$datePicker = $('<input type="text" id="datepicker_'+ idPrefix + this.uId +'" >');
         this.$datePickerWrapper = wrapInput(this.$datePicker, options.datepicker);
 
-        this.$timePicker = $('<input type="text" id="timepicker_'+ uId +'">');
+        this.$timePicker = $('<input type="text" id="timepicker_'+ this.uId +'">');
         this.$timePickerWrapper = wrapInput(this.$timePicker, options.timepicker);
 
         $picker.after(this.$timePickerWrapper).after(this.$datePickerWrapper);
 
-        if (options.distinct) {
-            $picker.addClass('hide')
-                .siblings('label')
-                    .addClass('hide');
-        } else {
-            this.$datePickerWrapper.addClass('hide');
-            this.$timePickerWrapper.addClass('hide');
-        }
+        this._optionRefresh('distinct');
 
-        if (options.min) {
-            options.datepicker.min = options.min[0];
-        }
-        if (options.max) {
-            options.datepicker.max = options.max[0];
-        }
+
         this.$datePicker.pickadate(options.datepicker);
         this.oDatePicker = this.$datePicker.pickadate('picker');
 
         this.$timePicker.pickatime(options.timepicker);
         this.oTimePicker = this.$timePicker.data('clockpicker');
 
+        aRestrinction.forEach(function(v) {
+            if (options[v]) {
+                self.dispatchDateOption(v);
+            }
+        });
+
         // open datepicker on datetimepicker input click
         $picker.on('focus', function() {
             self.oDatePicker.open();
         });
+        function bindValue(datetime) {
+            if (datetime) {
+                var aSplit = /(.*)\s+(\d{1,2}:\d{1,2}\s*(AM|PM)?)$/gi.exec(datetime);
+                if (aSplit) {
+                    if (aSplit[1]) {
+                        self.oDatePicker.set('select', aSplit[1]);
+                        self.$datePicker.val(aSplit[1]).trigger('change');
+                    }
+                    if (aSplit[2]) {
+                        self.$timePicker.val(aSplit[2]).trigger('change');
+                    }
+                }
+            }
+            datetime = $picker.val();
+            var date = self.$datePicker.val(), time = self.$timePicker.val();
+
+            if (date && time) {
+                $picker.val(date+' '+time);
+            } else {
+                $picker.val('');
+            }
+
+            if ($picker.val() !== datetime)
+                $picker.trigger('change');
+        }
+        bindValue($picker.val());
         // switch to Timepicker on close
         this.oDatePicker
             .on('set', function(val) {
-                val = new Date(val.select);
-                val = [val.getFullYear(), val.getMonth(), val.getDate()];
-                self.aDate = val;
+                if (val.select) {
+                    val = new Date(val.select);
+                    val = [val.getFullYear(), val.getMonth(), val.getDate()];
+                    self.aDate = val;
+                } else {
+                    self.aDate = [];
+                }
+                bindValue();
             })
             .on('close', function() {
-                self.dispatchTimeOption('min');
-                self.dispatchTimeOption('max');
-                self.dispatchTimeOption('disable');
-                if (options.step) {
-                    self.oTimePicker.show();
+                if (self.aDate.length) {
+                    self.dispatchTimeOption('min');
+                    self.dispatchTimeOption('max');
+                    self.dispatchTimeOption('disable');
+                    if (options.step) {
+                        self.oTimePicker.show();
+                    }
                 }
+                bindValue();
+            });
+        this.$timePicker
+            .on('change', function() {
+                bindValue();
             });
     }
 
@@ -117,9 +151,9 @@
         distinct: true,             // Double picker : true, single picker : false
         step: true,                 // Got to timepicker after pickadate
 
-        min: false,                 // format : [{pickadate.min},[h,m]]
-        max: false,                 // format : [{pickadate.min},[h,m]]
-        disable: false,             // format : [[{pickadate.min},[h,m]], [{pickadate.min},[h,m]], ... ]
+        min: false,                 // format : [{pickadate.min}, {pickatime.min}]
+        max: false,                 // format : [{pickadate.max}, {pickatime.max}]
+        disable: false              // format : [[{pickadate.format},{pickatime.format}], [{pickadate.format},[{pickatime.format}, {pickatime.format}, ...]], ... ]
     };
 
     DateTimePicker.prototype.dispatchTimeOption = function (optName) {
@@ -137,8 +171,7 @@
                     // step 2 : list disabled time
                     var mapping = [];
                     aDate.forEach(function(v) {
-                        var concat = v[1].some(function(time) {
-                            console.log('is Array ?', time);
+                        v[1].forEach(function(time) {
                             if (Array.isArray(time)) {
                                 mapping = mapping.concat(v[1]);
                                 return true;
@@ -148,20 +181,86 @@
                             }
                         });
                     });
-                    console.log('disable h:', mapping);
-                    this.oTimePicker.options[optName] = mapping;
+                    if (options.timepicker[optName]) {
+                        mapping.concat(options.timepicker[optName]);
+                    }
+                    this.oTimePicker.set(optName, mapping);
                 } else {
-                    this.oTimePicker.options[optName] = options.timepicker[optName];
+                    this.oTimePicker.set(optName, options.timepicker[optName]);
                 }
             } else {
                 if (isEqual(this.aDate, opt[0])) {
-                    this.oTimePicker.options[optName] = opt[1];
+                    this.oTimePicker.set(optName, opt[1]);
                 } else {
-                    this.oTimePicker.options[optName] = options.timepicker[optName];
+                    this.oTimePicker.set(optName, options.timepicker[optName]);
                 }
             }
         }
-    }
+    };
+    DateTimePicker.prototype.dispatchDateOption = function(optName) {
+        var options = this.options;
+        var opt = options[optName];
+
+        if (optName === 'disable') {
+            var mapping = opt.map(function(v) {
+                return v[0];
+            });
+            if (options.datepicker[optName]) {
+                mapping.concat(options.datepicker[optName]);
+            }
+            this.oDatePicker.set(optName, mapping);
+        } else {
+            if (Array.isArray(opt)) {
+                this.oDatePicker.set(optName, opt[0]);
+            }
+        }
+    };
+
+    DateTimePicker.prototype._optionRefresh = function(optName) {
+        var options = this.options;
+        var $picker = this.element;
+        if (optName === 'datepicker') {
+            this.oTimePicker.set(options.datepicker);
+
+        }
+        if (optName === 'timepicker') {
+            this.oTimePicker.set(options.timepicker);
+
+        }
+        if (optName === 'distinct') {
+            // if (options.distinct) {
+            //     $picker.addClass('hide')
+            //         .siblings('label')
+            //             .addClass('hide');
+            //     this.$datePickerWrapper.removeClass('hide');
+            //     this.$timePickerWrapper.removeClass('hide');
+            // } else {
+            //     this.$datePickerWrapper.addClass('hide');
+            //     this.$timePickerWrapper.addClass('hide');
+            //     $picker.removeClass('hide')
+            //         .siblings('label')
+            //         .removeClass('hide');
+            // }
+        }
+        if (aRestrinction.indexOf(optName) !== -1) {
+            this.dispatchDateOption(optName);
+        }
+    };
+    DateTimePicker.prototype.set = function (optName, value) {
+        var self = this;
+        if ($.isPlainObject(optName)) {
+            $.each(optName, function(k, v) {
+                self.set(k, v);
+            });
+        } else {
+            if (optName === 'datepicker' || optName === 'timepicker') { // use extend for datetime and timepicker subconfiguration
+                this.options[optName] = $.extend({}, this.options[optName], value);
+            } else {
+                this.options[optName] = value;
+            }
+            this._optionRefresh(optName);
+        }
+    };
 
     DateTimePicker.prototype.remove = function() {
         this.element.removeData('datetimepicker');
@@ -171,15 +270,15 @@
 
         this.$datePickerWrapper.remove();
         this.$timePickerWrapper.remove();
-    }
+    };
 
     // Extends $.fn.clockpicker
     $.fn.pickadatetime = function(option){
         var args = Array.prototype.slice.call(arguments, 1);
 
-        var componentData = this.data( 'datetimepicker' )
+        var componentData = this.data( 'datetimepicker' );
 
-        if ( option === 'picker' ) {
+        if ( componentData && (option === 'picker' || option === undefined)) {
             return componentData
         }
 
@@ -188,12 +287,20 @@
             var $this = $(this),
                 data = $this.data('datetimepicker');
             if (!data) {
-                options = $.extend({}, DateTimePicker.DEFAULTS, $this.data(), typeof option === 'object' && option);
+                var opt = $.extend({}, $this.data(), typeof option === 'object' && option);
+                if (opt.timepicker) {
+                    $.extend(opt.timepicker, DateTimePicker.DEFAULTS.timepicker, opt);
+                    console.log(opt);
+                }
+                if (opt.datepicker) {
+                    $.extend(opt.datepicker, DateTimePicker.DEFAULTS.datepicker, opt);
+                }
+                options = $.extend({}, DateTimePicker.DEFAULTS, opt);
+
                 $this.data('datetimepicker', new DateTimePicker($this, options));
             } else {
                 if ($.isPlainObject(option)) { // case recall with new parameters
-                    options = $.extend({}, data.options, option); // merge previous options
-                    data.options = options;
+                    data.set(option);
                 }
                 // Manual operatsions. show, hide, remove, e.g.
                 if (typeof data[option] === 'function')
@@ -201,4 +308,5 @@
             }
         });
     };
-})(jQuery, Picker)
+    $.fn.pickadatetime.defaults = DateTimePicker.DEFAULTS;
+})(jQuery);
